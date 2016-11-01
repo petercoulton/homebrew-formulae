585aefec753eb1313e242f66ca72b0d3450a5644
Peter Coulton <petercoulton@gmail.com>
2016-11-01 03:04:39 +0000

    (Fix) Update laptop url

--- Formula/laptop.rb
+++ Formula/laptop.rb
@@ -1,7 +1,7 @@
 class Laptop < Formula
   desc "Collection of scripts"
   homepage "https://github.com/petercoulton/laptop"
-  url "https://github.com/petercoulton/laptop/archive/v0.0.1.tar.gz"
+  url "https://github.com/petercoulton/laptop/archive/0.0.1.tar.gz"
   version "0.0.1"
   sha256 "5d1930a9033a4d6a178cbfc9187820df44337437bc950e9347a505730eb6349c"
 
