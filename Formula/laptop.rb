493f7014c82414d4739370aa286d0b33f28d1543
Peter Coulton <petercoulton@gmail.com>
2016-11-01 04:02:56 +0000

    (Update) Laptop release 0.0.2

--- Formula/laptop.rb
+++ Formula/laptop.rb
@@ -1,15 +1,15 @@
 class Laptop < Formula
   desc "Collection of scripts"
   homepage "https://github.com/petercoulton/laptop"
-  url "https://github.com/petercoulton/laptop/archive/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "8602ade98e62e3b183f5625b3b44a3aa936c5d4f235d4331188dcf0c89a00602"
+  url "https://github.com/petercoulton/laptop/archive/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "60715df9999e66c75c436e704e275f4d10c7c804336cdae65980ea149a9af708"
 
   def install
-  	prefix.install 'bin', 'lib'
+    prefix.install 'bin', 'lib'
   end
 
   test do
-  	system "#{bin}/dotfiles", '--help'
+    system "#{bin}/dotfiles", '--help'
   end
 end
