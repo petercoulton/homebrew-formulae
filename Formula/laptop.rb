603cb6c4e08bd39279d0e11e548a32943a5dc10b
Peter Coulton <petercoulton@gmail.com>
2016-11-01 02:46:21 +0000

    (Fix) Update Laptop sha again

--- Formula/laptop.rb
+++ Formula/laptop.rb
@@ -3,7 +3,7 @@ class Laptop < Formula
   homepage "https://github.com/petercoulton/laptop"
   url "https://github.com/petercoulton/laptop/archive/v0.0.1.tar.gz"
   version "0.0.1"
-  sha256 "9be22ee84fcaf6874092bef7fee216ea837e1e49294a774ecb65f09efe40a96f"
+  sha256 "5d1930a9033a4d6a178cbfc9187820df44337437bc950e9347a505730eb6349c"
 
   def install
   	prefix.install 'bin', 'lib'
