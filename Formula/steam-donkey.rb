9dab73b4e7758f4aaa9439c6ec12bdc21fbc1084
Peter Coulton <petercoulton@gmail.com>
2016-11-08 14:02:12 +0000

    Update steam-donkey

--- Formula/steam-donkey.rb
+++ Formula/steam-donkey.rb
@@ -1,9 +1,9 @@
 class SteamDonkey < Formula
   desc "Tools and scripts for building and managing infrastructure on AWS."
   homepage "https://github.com/petercoulton/steam-donkey"
-  url "https://github.com/petercoulton/steam-donkey/archive/v0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "d87b9d637304eeccdd45562eb3f52671b74fb899bddf1120c87a9eb73d53924b"
+  url "https://github.com/petercoulton/steam-donkey/archive/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "f764ce5f8b8cda5e5e5657c717708947777f380c9d9f69f31b0fd03dc68b4e53"
 
   def install
     prefix.install 'bin', 'lib'
